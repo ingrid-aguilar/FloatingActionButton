5979accb2f96b71a988b6620d84edf2826cfe079
ingrid <ingrid.aguilar@cbtis72.edu.mx>
2019-06-05 21:00:10 -0500

    ROTAR FLOTING ACTION BUTTON AL CLICKEARLO

--- app/src/main/java/com/example/floatingactionbutton/MainActivity.java
+++ app/src/main/java/com/example/floatingactionbutton/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.animation.Interpolator;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean click = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,53 +23,67 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        //Animación de escala en el floating action button
+        //ANIMACIÓN DE ESCALA EN EL FLOATING ACTION BUTTON
 
        final FloatingActionButton fab = findViewById(R.id.fab);
-       fab.setScaleX(0);
-       fab.setScaleY(0);
-       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-           final Interpolator interpolator = AnimationUtils.loadInterpolator(getBaseContext(), android.R.interpolator.fast_out_slow_in);
+      // fab.setScaleX(0);
+       //fab.setScaleY(0);
+      // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+         //  final Interpolator interpolator = AnimationUtils.loadInterpolator(getBaseContext(), android.R.interpolator.fast_out_slow_in);
 
-           fab.animate()
-                   .scaleX(1)
-                   .scaleY(1)
-                   .setInterpolator(interpolator)
-                   .setDuration(600)
-                   .setStartDelay(1000)
-                   .setListener(new Animator.AnimatorListener() {
-                       @Override
-                       public void onAnimationStart(Animator animation) {
+           //fab.animate()
+             //      .scaleX(1)
+               //    .scaleY(1)
+                 //  .setInterpolator(interpolator)
+                   //.setDuration(600)
+                   //.setStartDelay(1000)
+                   //.setListener(new Animator.AnimatorListener() {
+                     //  @Override
+                       //public void onAnimationStart(Animator animation) {
 
-                       }
+                       //}
 
-                       @Override
-                       public void onAnimationEnd(Animator animation) {
-                           fab.animate()
-                                   .scaleY(0)
-                                   .scaleX(0)
-                                   .setInterpolator(interpolator)
-                                   .setDuration(600)
-                                   .start();
+                     //  @Override
+                      // public void onAnimationEnd(Animator animation) {
+                        //   fab.animate()
+                          //         .scaleY(0)
+                            //       .scaleX(0)
+                              //     .setInterpolator(interpolator)
+                               //    .setDuration(600)
+                                 //  .start();
 
-                       }
+                      // }
 
-                       @Override
-                       public void onAnimationCancel(Animator animation) {
+//                       @Override
+        //                     public void onAnimationCancel(Animator animation) {
 
-                       }
+//                       }
 
-                       @Override
-                       public void onAnimationRepeat(Animator animation) {
+  //                     @Override
+    //                   public void onAnimationRepeat(Animator animation) {
 
-                       }
-                   });
-       }
+      //                 }
+        //           });
+      // }
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                //ROTAR FLOTING ACTION BUTTON AL CLICKEARLO
+
+                click = !click;
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    final Interpolator interpolator = AnimationUtils.loadInterpolator(getBaseContext(), android.R.interpolator.fast_out_slow_in);
+
+                    view.animate()
+                            .rotation(click ? 45f : 0)
+                            .setInterpolator(interpolator)
+                            .start();
+                }
+
+              //  Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
             }
         });
     }
